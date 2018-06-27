f33ba3777c92d798991af689cb24a1be3ad4a6a0
Alicia Centeno <alicent.08.02@gmail.com>
2018-06-27 17:44:14 -0400

    se realiza la funcion fetch y css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-/* Holiii acá va tu código también */
+document.getElementById('btnEnviarConsulta').addEventListener('click', cargarJson);
+
+function cargarJson() {
+	const numeroTarjetaBip = document.getElementById('numeroTarjeta').value
+	console.log(numeroTarjetaBip)
+	let url = '';
+	url += 'http://www.psep.cl/api/Bip.php?&'
+	url += `numberBip=${numeroTarjetaBip}`
+	fetch(url)
+		.then(resp => resp.json())
+		.then(data => {
+			console.log(data)
+		})
+		.catch(e => console.log(e))
+}
